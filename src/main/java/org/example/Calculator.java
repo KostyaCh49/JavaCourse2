625f19ff5bc49a1a959ea92f77360931c07d90d8
kostyafox49 <137185267+kostyafox49@users.noreply.github.com>
2023-09-30 21:21:04 +0300

    Calculator
    Calculator

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNum = scanner.nextInt();
+        char operator = scanner.next().charAt(0);
+        int secondNum = scanner.nextInt();
+
+        int result = 0;
+        if (operator == '+') {
+            result = firstNum + secondNum;
+        } else if (operator == '-') {
+            result = firstNum - secondNum;
+        } else if (operator == '*') {
+            result = firstNum * secondNum;
+        } else if (operator == '/') {
+            if (secondNum == 0) {
+                System.out.println("Can't divide by zero!");
+            } else {
+                result = firstNum / secondNum;
+            }
+        } else {
+            System.out.println("/=");
+        }
+
+        System.out.print("Result: " + result);
+    }
+}
